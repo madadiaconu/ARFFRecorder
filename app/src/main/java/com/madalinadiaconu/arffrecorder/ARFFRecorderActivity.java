6b8b3a299864a18d52dfb24988a6ab8d1e5446e4
madadiaconu <maddaaa04@yahoo.com>
2016-10-14 13:25:55 +0200

    Re-work UI

--- app/src/main/java/com/madalinadiaconu/arffrecorder/ARFFRecorderActivity.java
+++ app/src/main/java/com/madalinadiaconu/arffrecorder/ARFFRecorderActivity.java
@@ -1,50 +1,53 @@
 package com.madalinadiaconu.arffrecorder;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
 import android.widget.CompoundButton;
 import android.widget.TextView;
-import android.widget.ToggleButton;
 
 import de.greenrobot.event.EventBus;
 
 /**
  * Created by Diaconu Madalina on 12.10.2016.
+ * Activity used to display the data from the accelerometer
  */
 public class ARFFRecorderActivity extends AppCompatActivity{
 
     private TextView xCoordinate, yCoordinate, zCoordinate;
 
     @Override
+    @SuppressWarnings("ConstantConditions")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arffrecorder);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_title);
+        toolbar.setTitleTextColor(Color.WHITE);
         setSupportActionBar(toolbar);
 
         xCoordinate = (TextView) findViewById(R.id.x_coordinate);
         yCoordinate = (TextView) findViewById(R.id.y_coordinate);
         zCoordinate = (TextView) findViewById(R.id.z_coordinate);
 
-        ToggleButton toggleButton = (ToggleButton) findViewById(R.id.fab);
+        SwitchCompat switchButton = (SwitchCompat) findViewById(R.id.fab);
         final Intent serviceIntent = new Intent(ARFFRecorderActivity.this, ARFFRecorderService.class);
 
-        if (toggleButton != null) {
-            toggleButton.setChecked(ARFFRecorderService.isOn());
-            toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    if (isChecked) {
-                        startService(serviceIntent);
-                    } else {
-                        stopService(serviceIntent);
-                    }
+        switchButton.setChecked(ARFFRecorderService.isOn());
+        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    startService(serviceIntent);
+                } else {
+                    stopService(serviceIntent);
                 }
-            });
-        }
+            }
+        });
     }
 
     @Override
@@ -60,8 +63,8 @@ public class ARFFRecorderActivity extends AppCompatActivity{
     }
 
     public void onEvent(AccelerometerInfo event){
-        xCoordinate.setText(String.valueOf(event.getX()));
-        yCoordinate.setText(String.valueOf(event.getY()));
-        zCoordinate.setText(String.valueOf(event.getZ()));
+        xCoordinate.setText(String.format("x = %.2f", event.getX()));
+        yCoordinate.setText(String.format("y = %.2f", event.getY()));
+        zCoordinate.setText(String.format("z = %.2f", event.getZ()));
     }
 }
